caa36997bb353ac88144b007dca5d9b506683c22
unknown <aleksandarristov28@gmail.com>
2023-12-23 00:17:53 +0100

    first java program

---
+++ src/BezReturn.java
@@ -0,0 +1,16 @@
+
+public class BezReturn {
+
+	public static void main(String[] args) {
+		
+		myFunc(10);
+		
+		
+	}
+	
+	public static void myFunc(int x) {
+		int y=x*3;
+		System.out.println(y);
+	}
+
+}
